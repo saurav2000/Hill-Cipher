01db7a9ab0f010376a9b4d598b83bf851a5c80f5
Saurav <musunurusaurav@gmail.com>
2020-01-16 23:18:02 +0530

    Encryption done. Decryption?

--- hill_cipher_crypto.cpp
+++ hill_cipher_crypto.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+int k;
+
 bool verify(string s)
 {
 	for(int i=0;i<s.length();++i)
@@ -15,18 +17,42 @@ bool verify(string s)
 	return false;
 }
 
+int determinant(int **m, )
+{
+	return 0;
+}
 
+// bool inverse()
+
+int extendedEuclid(int a, int b, int &x, int &y)
+{
+	if(!a)
+	{
+		x = 0;
+		y = 1;
+		return b;
+	}
+
+	int x_,y_;
+	int g = extendedEuclid(b%a, a, x_, y_);
+
+	x = y_ - (b/a)*x_;
+	y = x_;
+
+	return g;
+}
 
 int main()
 {
-	int k;
 	cout<<"Enter the key size: \n";
 	cin>>k;
 	cout<<"Enter the key: \n";
 
-	int key[k][k];
+	int **key = (int**)malloc(sizeof(int*) * k);
+	for(int i=0;i<k;++i)
+		key[i] = (int*)malloc(sizeof(int) * k);
 	string text;
-
+	determinant(key);
 	for(int i=0;i<k;++i)
 		for(int j=0;j<k;++j)
 			cin>>key[i][j];
@@ -39,6 +65,25 @@ int main()
 		flag = verify(text); 
 	}
 
+	if(text.length()%k!=0)
+	{
+		for(int i=0;i<k-(text.length()%k);++i)
+			text+= "x";
+	}
 
+	string encrypted = "";
+	for(int i=0;i*k<text.length();i+=1)
+	{
+		for(int l=0;l<k;++l)
+		{
+			int sum = 0;
+			for(int m=0;m<k;++m)
+				sum+= key[l][m]*(text[i*k+m]-97);
+			encrypted+= ((char)((sum%N)+97));
+		}
+	}
+
+	cout<<text<<"\n\n";
+	cout<<encrypted<<"\n";
 
 }
