505a23a9e8c2097a36b8499ecde369597802af15
Saurav <musunurusaurav@gmail.com>
2020-01-26 19:03:09 +0530

    Merged analysis into hill_cipher.py

--- hill_cipher.py
+++ hill_cipher.py
@@ -7,7 +7,7 @@ def euclid(a, b):
 	(g, x, y) = euclid(b%a, a)
 	return (g, y - ((b//a) * x), x)
 
-def inverse(a, m):
+def mod_inverse(a, m):
 	a = (a%m + m)%m
 	(g, x, y) = euclid(a, m)
 	if(g!=1):
@@ -21,7 +21,10 @@ def cryption(text, key, k):
 		l+= [x+97 for x in res]
 	return list(l)
 
-def main():
+def textToMatrix(text_list):
+	return sp.Matrix([[ord(x)-97 for x in s] for s in text_list])
+
+def cryptionMain():
 	# Reading lines
 	file = open(sys.argv[1])
 	lines = file.readlines()
@@ -36,10 +39,10 @@ def main():
 		print("Key size improper")
 		return
 
-	# Checking determinant of key and computing key_inverse & padding text
+	# Checking determinant of key and computing key_mod_inverse & padding text
 	key = sp.Matrix(k, k, [int(i) for i in key_line])
 	det = key.det()
-	det_inv = inverse(det, 26)
+	det_inv = mod_inverse(det, 26)
 	if(det_inv==-1):
 		print("Key det improper")
 		return
@@ -55,6 +58,36 @@ def main():
 	file = open(sys.argv[3], "w")
 	print(final_text, file=file)
 	file.close()
+
+def cryptanalysis():
+	# Reading text
+	file = open(sys.argv[1])
+	cipher_text = file.read()[:-1]
+	file.close()
+	frequency_map = {}
+	for i in range(0, len(cipher_text), 2):
+		sub = cipher_text[i:i+2]
+		if sub in frequency_map:
+			frequency_map[sub]+=1
+		else:
+			frequency_map[sub]=1
+
+	top_freq = sorted(frequency_map, key = frequency_map.get, reverse=True)[:4]
+	top_eng_digraphs = ['th', 'he']
+
+	eng_text_inverse = textToMatrix(top_eng_digraphs).inv_mod(26)
+
+	for i in range(4):
+		for j in range(4):
+			if i==j:
+				continue;
+			key = (textToMatrix([top_freq[i], top_freq[j]]) * eng_text_inverse) % 26
+
+def main():
+	if(int(sys.argv[2])==2):
+		cryptanalysis()
+	else:
+		cryptionMain()
 
 if __name__ == '__main__':
 	main()
